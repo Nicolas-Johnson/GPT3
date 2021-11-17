79fd79d2be4e178cdf599eb6eaccbf3a8cf157b3
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-17 15:08:53 -0300

    App css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
 import { Brand, CTA, Navbar} from './components';
+import './App.css';
 
 export default function App() {
   return (
